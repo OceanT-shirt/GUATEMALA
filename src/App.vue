57ceec825bc4f54a75d2816bcfe5ef1c860f85c5
OceanT-shirt <stylish.trousers@gmail.com>
2022-04-20 16:16:55 +0900

    Add: text

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="WELCOME TO CLOUD LEAGUE" text="We are focusing on WEB3"/>
 </template>
 
 <script>
